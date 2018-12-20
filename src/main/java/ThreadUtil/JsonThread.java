f27a8c2ef36b7e955956973e0653716de0b3c9f5
Msopopop <l92412wow@gmail.com>
2018-12-20 23:27:21 +0800

    Delete All Jpg Parse Methods
    Add JsonThread.java -->Json2DCM
    
    Signed-off-by: Msopopop <l92412wow@gmail.com>

--- src/main/java/ThreadUtil/JsonThread.java
+++ src/main/java/ThreadUtil/JsonThread.java
@@ -10,20 +10,20 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.time.LocalDate;
 
-public class DicomThread implements Runnable {
+public class JsonThread implements Runnable {
 
     public static String HDFS_NODE_NAME;
     public static String HBASE_ZOOKEEPER_QUORUM;
     public static String TABLE_NAME;
-    private static Logger logger = Logger.getLogger(DicomThread.class);
-    private static String FILE_ROOT_DIR;
     public static String HDFS_ROOT_DIR;
+    private static Logger logger = Logger.getLogger(JsonThread.class);
+    private static String FILE_ROOT_DIR;
     private static WatchService FileWatchService;
     private static HDFSUtil hdfsUtil;
     private static HBaseUtil HBaseUtil;
 
 
-    public DicomThread() {
+    public JsonThread() {
         try {
             FileWatchService = FileSystems.getDefault().newWatchService();
 
@@ -33,46 +33,29 @@ public class DicomThread implements Runnable {
     }
 
     public void setFilePath(String FILE_ROOT_DIR) throws IOException {
-        DicomThread.FILE_ROOT_DIR = FILE_ROOT_DIR;
+        JsonThread.FILE_ROOT_DIR = FILE_ROOT_DIR;
         Paths.get(FILE_ROOT_DIR).register(FileWatchService, StandardWatchEventKinds.ENTRY_CREATE);
     }
 
     @Override
     @SuppressWarnings("static-access")
     public void run() {
-        try {
-            this.runDicom();
-        } catch (IOException | InterruptedException e) {
-            logger.error(e.toString());
-        }
+
     }
 
     @SuppressWarnings("static-access")
-    private void runDicom() throws IOException, InterruptedException {
-        hdfsUtil = new HDFSUtil(HDFS_NODE_NAME);
-        HBaseUtil = new HBaseUtil(HBASE_ZOOKEEPER_QUORUM);
+    private void runDicom(File GSPSFile) throws IOException, InterruptedException {
         while (true) {
             WatchKey watchKey = FileWatchService.take();
             for (WatchEvent<?> event : watchKey.pollEvents()) {
-                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE
-                        && event.context().toString().endsWith(".fin")) {
-
+                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE && event.context().toString().endsWith(".fin")) {
                     String Date = LocalDate.now().toString(); // "2018-12-25"
-                    String FileExName = event.context().toString(); // "a.dcm.fin"
-                    String FileName = FileExName.substring(0, FileExName.length() - 4);
-
-                    String FullFileName = FILE_ROOT_DIR + FileName; // "home/xxx/a.dcm"
-                    File dcmFile = new File(FullFileName);
-
-                    // Create a new dir classified by date
-                    // hdfs://${HDFS_NODE_NAME}:9000/dicomFile/yyyy-mm-dd
                     hdfsUtil.mkdir(HDFS_ROOT_DIR + Date);
-                    // Upload files to hdfs://master:9000/dicomFile/yyyy-mm-dd
-                    hdfsUtil.uploadFile(dcmFile, HDFS_ROOT_DIR + Date);
+                    // Upload files to hdfs://master:9000/GSPSFile/yyyy-mm-dd
+                    hdfsUtil.uploadFile(GSPSFile, HDFS_ROOT_DIR + Date);
 
-                    AttrUtil attrUploadUtil = new AttrUtil(dcmFile);
-                    attrUploadUtil.UploadToHBase(HBaseUtil, TABLE_NAME, HDFS_ROOT_DIR, Date, true);
-
+                    AttrUtil attrUploadUtil = new AttrUtil(GSPSFile);
+                    attrUploadUtil.UploadToHBase(HBaseUtil, TABLE_NAME, HDFS_ROOT_DIR, Date, false);
                 }
             }
             watchKey.reset();
--- src/main/java/mainClass.java
+++ src/main/java/mainClass.java
@@ -1,10 +1,11 @@
 import ThreadUtil.DicomThread;
+import ThreadUtil.JsonThread;
 import Utils.XMLUtil;
 import org.apache.log4j.Logger;
 
 public class mainClass {
     private static Logger logger = Logger.getLogger(mainClass.class);
-    private static final String GSPS_ROOT_DIR = "/home/verizonwu/GSPSFile/";
+    private static final String JSON_ROOT_DIR = "/home/verizonwu/JSONFile/";
     private static final String DICOM_ROOT_DIR = "/home/hadoop/dicomFile/";
 
     private static final String HDFS_ROOT_DIR_DICOM = "/dicomFile/";
@@ -27,28 +28,30 @@ public class mainClass {
         //TODO Not finished yet -- set all Strings
         xmlUtil.parseParameters();
 
-        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         DicomThread.HDFS_NODE_NAME = HDFS_NODE_NAME;
         DicomThread.HBASE_ZOOKEEPER_QUORUM = HBASE_ZOOKEEPER_QUORUM;
         DicomThread.TABLE_NAME = TABLE_NAME;
+        DicomThread.HDFS_ROOT_DIR = HDFS_ROOT_DIR_DICOM;
+
+        JsonThread.HDFS_NODE_NAME = DicomThread.HDFS_NODE_NAME;
+        JsonThread.HBASE_ZOOKEEPER_QUORUM = DicomThread.HBASE_ZOOKEEPER_QUORUM;
+        JsonThread.TABLE_NAME = DicomThread.TABLE_NAME;
+        JsonThread.HDFS_ROOT_DIR = HDFS_ROOT_DIR_GSPS;
 
         DicomThread dicomThread = new DicomThread();
-        DicomThread gspsThread = new DicomThread();
+        JsonThread jsonThread = new JsonThread();
 
         dicomThread.setFilePath(DICOM_ROOT_DIR);
-        gspsThread.setFilePath(GSPS_ROOT_DIR);
-
-        dicomThread.setParameters(HDFS_ROOT_DIR_DICOM, true);
-        gspsThread.setParameters(HDFS_ROOT_DIR_GSPS, false);
+        jsonThread.setFilePath(JSON_ROOT_DIR);
 
         dicomThread.run();
-        gspsThread.run();
+        jsonThread.run();
 
         // Close listeners thread before main thread exit
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
                 dicomThread.close();
-                gspsThread.close();
+                jsonThread.close();
             } catch (Exception e) {
                 logger.error(e.toString());
             }
